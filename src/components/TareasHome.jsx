5adb3cb128e7cfe166c567e32e38e8e6d1a62b3b
KevinQuesada251 <kquesada988@gmail.com>
2025-03-08 20:57:57 -0600

    filtro de usuarios por tarea

--- src/components/TareasHome.jsx
+++ src/components/TareasHome.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import Swal from 'sweetalert2'
 import llamados from '../services/llamados'
@@ -7,6 +7,22 @@ import "../styles/TareasHome.css"
 function TareasHome() {
 
   let nombreUsuario = localStorage.getItem("NombreUsuario")
+
+  useEffect(() => {
+
+  async function mostrar() {
+    const todasLasTareas = await llamados.getUsers("tareas")
+    const tareasPorUsuario = todasLasTareas.filter(persona=> persona.nombreUsuario === nombreUsuario)
+    console.log(tareasPorUsuario);
+    
+  }
+
+  mostrar()
+},[])
+  
+
+
+
   
     async function enviarNuevaTarea(){
     const { value: formValues } = await Swal.fire({
@@ -29,16 +45,18 @@ function TareasHome() {
         "tarea":document.getElementById("tareaInfo").value
       }
        llamados.postUsers( nueva,"tareas")
-       console.log(nueva);
+
     }
   }
+
+ 
 
 
   return (
     <div className='contenedorTareas'>
         <h1>Bienvenido a tu lista de tareas <span>{nombreUsuario}</span></h1>
         <div className='tarea'>
-            
+            <p></p>
 
             <button onClick={enviarNuevaTarea}>Nueva Tarea</button>
 
